71cdd341cc3c4259142f89905d6b61cb05dc65fe
mitra <mitra@LAPTOP-SAYANTANM3>
2021-05-27 09:02:07 +0530

    Hackathon

--- Outstation_Cab/src/main/java/libraries/ScreenshotManager.java
+++ Outstation_Cab/src/main/java/libraries/ScreenshotManager.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebDriver;
 public class ScreenshotManager{
 	
 	public static void screenshot(WebDriver driver, String name) throws IOException{ 
-		String filepath = "C:\\Users\\mitra\\eclipse-workspace\\Cab_Booking\\Screenshots\\";
+		String filepath = System.getProperty("user.dir")+"\\Screenshots\\";
 		//creating src file instance for storing the screenshot using TakesScreenshot interface
 		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);  
 		File dest = new File(filepath + name + ".png");
